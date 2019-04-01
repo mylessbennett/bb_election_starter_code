039996abc585a64e5b48e67120ef99e13497efb1
Myles Bennett <myles.bennett@hotmail.com>
2019-04-01 17:18:53 -0400

    implemented ability to vote synchronously/ added to stylesheet

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -2,10 +2,22 @@ document.addEventListener("DOMContentLoaded", function() {
     const request = axios.get('https://bb-election-api.herokuapp.com/')
     .then(function(response) {
             response.data.candidates.forEach(function(candidate) {
-            const listItem = document.createElement('li')
-            listItem.innerText = `Candidate: ${candidate.name} \n Votes: ${candidate.votes}`
-            candidatesList = document.querySelector('.candidates')
-            candidatesList.appendChild(listItem)
+            const listItem = document.createElement('li');
+            listItem.innerText = `Candidate: ${candidate.name} \n Votes: ${candidate.votes}`;
+            candidatesList = document.querySelector('.candidates');
+            candidatesList.appendChild(listItem);
+            const voteForm = document.createElement('form');
+            voteForm.method = "POST";
+            voteForm.action = "https://bb-election-api.herokuapp.com/vote"
+            listItem.appendChild(voteForm);
+            const submitButton = document.createElement('button');
+            submitButton.innerText = 'Submit';
+            voteForm.appendChild(submitButton);
+            const hiddenInput = document.createElement('input');
+            hiddenInput.type = "hidden";
+            hiddenInput.name = "name";
+            hiddenInput.value = candidate.name;
+            voteForm.appendChild(hiddenInput);
         })
     })
 });
