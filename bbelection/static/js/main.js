9302c804e818c05c21b1b38c9f0ae83ec94ab946
Myles Bennett <myles.bennett@hotmail.com>
2019-04-01 14:53:02 -0400

    displayed name and votes for each candidate/ added styles

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    const request = axios.get('https://bb-election-api.herokuapp.com/')
+    .then(function(response) {
+            response.data.candidates.forEach(function(candidate) {
+            const listItem = document.createElement('li')
+            listItem.innerText = `Candidate: ${candidate.name} \n Votes: ${candidate.votes}`
+            candidatesList = document.querySelector('.candidates')
+            candidatesList.appendChild(listItem)
+        })
+    })
 });
